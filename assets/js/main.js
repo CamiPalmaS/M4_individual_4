fbd591997fe0e60c14b8c74e29911a5ed58ad9f6
CamiPalmaS <capalmasoto@gmail.com>
2024-08-19 17:15:52 -0400

    funcion para obtener info desde el archivo pokemon.js y se implementa funcion para mostrar la info obtenida mediante input usuario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,34 @@
 //importar informacion de los pokemon
-import { data } from "./pokemon";
+import { data } from "./pokemon.js";
+
+//funcion para obtener datos del pokemon desde data
+const extaerInfo = (data, identificador, callback) => {
+    setTimeout(() =>{
+        let info;
+        //typeof devuelve tipo de dato string/number/boleano?
+        if (typeof identificador === "number"){
+            info = data.find(pokemon => pokemon.id === identificador);
+        } else {
+            info = data.find(pokemon => pokemon.name.toLowerCase() === identificador.toLowerCase());
+        }
+
+        if (info){
+            callback(`ID: ${info.id}, Nombre: ${info.name}, Tipo: ${info.types.join(", ")}`);
+        } else {
+            callback("No se encontró el Pokemon");
+        }
+    }, 500);
+};
+
+//funcion para buscar pokemon
+document.getElementById("search").addEventListener("submit", (event) =>{
+    event.preventDefault();//evitar envio por default
+    //obtener input usuario
+    const input = document.getElementById("pokeID").value.trim();
+    //llamar a la funcion que obtiene los datos
+    extaerInfo(data, isNaN(input) ? input : parseInt(input), (resultado) => {
+        document.getElementById("resultado").textContent = resultado ? JSON.stringify(resultado) : "No se encontró el pokemon";
+    });
+    document.getElementById("search").reset();
+});
 
